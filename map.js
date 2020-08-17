045b29c4e02583a23ea531ffa1d45ca3392b6aca
rajeshjaga <rajaga223@gmail.com>
2020-08-17 19:16:06 +0530

    init-commit

---
+++ map.js
@@ -0,0 +1,9 @@
+//An javascript in-built function for array/objects.
+
+
+let numberArray = [10, 20, 53, 32, 89];
+
+let anotherNumberArray = numberArray.map(number => { return number ** 2; }
+);
+console.log(anotherNumberArray);
+console.log(numberArray);
