81cdafe444ba2ed3acb6bf7a66c468d8244fa253
rajeshjaga <rajaga223@gmail.com>
2020-09-04 07:34:17 +0530

    Day 3: Falsy values and Switch (Conditionals) complete

--- Conditionals/conditionals.js
+++ Conditionals/conditionals.js
@@ -1,6 +1,7 @@
 console.clear();
 //conditionals.
 
+//if else conditionals
 if (true) {
     console.log(true);
 } else {
@@ -8,4 +9,20 @@ if (true) {
 }
 
 
-console.log('warning no text');
+var bestLang = 'javascript';
+
+//switch statement takes a value/variable to evaluate in case if it matches the condition
+switch (bestLang) {
+    case 'javascript':
+        console.log('YAY! javascript is the best');
+        break;
+    case 'java':
+        console.log('Okay java is best');
+        break;
+    case 'python':
+        console.log('Python might be not be the best');
+        break;
+    default:
+        console.log('There was nothing better');
+        break;
+}
