d965bc3ca0d3e2014addb0742de4eeabcc03b084
Rajesh jaga <Rajaga223@gmail.com>
2020-09-25 18:27:48 +0530

    Day 10: Event Listener

---
+++ event-handling/Background-color-changer/index.js
@@ -0,0 +1,7 @@
+let buttonColor = document.querySelector('button');
+const body = document.querySelector('body');
+buttonColor.addEventListener('click', colorChanger);
+function colorChanger() {
+    body.style.background = `rgb(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)})`;
+    console.log(`rgb(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)})`)
+}
---
+++ event-handling/about.js
@@ -0,0 +1,5 @@
+//event handling is recongnising a change of a element property or any other mechanism
+
+//1
+//in the background-color changer the event is triggeredwhen the element is clicked and a block of code changes the
+//color of body element this is a click event
